5989fa8f2d8790b4234e9528a0e91c3b4ab37bf8
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-23 12:01:56 +0200

    Oprettet en consumer/client som benytter SOAP CalBoxService. Consol vinduet i consumer viser udregned volumen og manglende side.

---
+++ CalBoxConsumer_Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalBoxConsumer_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new BoxCalServiceReference.BoxCalServiceClient("BasicHttpBinding_IBoxCalService"))
+            {
+                Console.WriteLine("Udregning af Boksen volumen:");
+                
+                Console.WriteLine("Boksens volumen er:" + client.GetVolume(2,2,2));
+
+                Console.WriteLine("Udregning af Boksens manglende side:");
+
+                Console.WriteLine("Boksens manglende side er:" + client.GetSide(8,2,2));
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
