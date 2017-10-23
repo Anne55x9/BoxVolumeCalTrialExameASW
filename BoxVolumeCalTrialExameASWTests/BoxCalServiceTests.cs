b54f68108fd6c04fc981bd871bcd288ec87db121
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-23 11:32:16 +0200

    Testet metodernes udregning i wcf clienten lokalt. Oprettet unit test projekt til metoderne i service klassen. Ikke default metoder.

---
+++ BoxVolumeCalTrialExameASWTests/BoxCalServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoxVolumeCalTrialExameASW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxVolumeCalTrialExameASW.Tests
+{
+    [TestClass()]
+    public class BoxCalServiceTests
+    {
+        [TestMethod()]
+        public void GetVolumeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
