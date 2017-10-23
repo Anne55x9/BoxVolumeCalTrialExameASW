8acf75dcccd8fc016766a6d1428986237a5f0b68
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-23 11:14:55 +0200

    Starte et wcf projekt og ændret navne på service klassen og interfaset.

--- BoxVolumeCalTrialExameASW/IBoxCalService.cs
+++ BoxVolumeCalTrialExameASW/IBoxCalService.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace BoxVolumeCalTrialExameASW
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBoxCalService" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IBoxCalService
     {
 
         [OperationContract]
